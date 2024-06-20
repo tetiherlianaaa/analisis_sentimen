914883fced5082d3c4c443e5e3d6779364faba33
Teti Herliana <tetiherlianaaa@gmail.com>
2024-06-20 16:06:46 +0700

    first commit

--- page/klasifikasiKnn.py
+++ page/klasifikasiKnn.py
@@ -82,8 +82,11 @@ def main():
 
         tfidf, labels = data['tfidf'].tolist(), data['polarity'].tolist()
 
+        # Pilih nilai K
+        k_value = st.slider("Pilih nilai K:", 1, 10, 3)
+
         train_tfidf, train_labels, test_tfidf, test_labels = train_test_split(tfidf, labels)
-        predictions = knn(train_tfidf, train_labels, test_tfidf, k=3)
+        predictions = knn(train_tfidf, train_labels, test_tfidf, k=k_value)
         accuracy = calculate_accuracy(test_labels, predictions)
         st.write(f"Akurasi: {accuracy * 100:.2f}%")
 
@@ -98,8 +101,7 @@ def main():
         plt.title('Confusion Matrix')
         st.pyplot(fig)
 
-        k_unit = 5
-        data['polarity_predicted'] = knn(train_tfidf, train_labels, tfidf, k=k_unit)
+        data['polarity_predicted'] = knn(train_tfidf, train_labels, tfidf, k=k_value)
 
         @st.cache_data
         def convert_df(df):
@@ -113,7 +115,7 @@ def main():
         if input_text:
             corpus = data[text_column].tolist()
             new_input = compute_tfidf_single(input_text, corpus)
-            new_prediction = knn(train_tfidf, train_labels, [new_input], k=k_unit)
+            new_prediction = knn(train_tfidf, train_labels, [new_input], k=k_value)
             st.write(f"Kalimat: {input_text}")
             st.write(f"Prediksi Polarity: {new_prediction[0]}")
 
