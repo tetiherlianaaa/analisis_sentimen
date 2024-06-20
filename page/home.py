bee77223894bffcc4a3081641c8396a3ce55bbfd
Teti Herliana <tetiherlianaaa@gmail.com>
2024-06-20 16:22:01 +0700

    first commit

--- page/home.py
+++ page/home.py
@@ -1,12 +1,8 @@
 import streamlit as st
     
 def main():
-    st.title("Beranda")
-    st.write("Selamat datang di aplikasi Analisis Sentimen! ")
-    st.markdown("""
-    Aplikasi ini dirancang untuk membantu dalam memahami opini dan sentimen dari teks, yakni komentar aplikasi. Dengan menggunakan algoritma K-Nearest Neighbors (KNN) dan Naive Bayes, aplikasi ini dapat memberikan informasi terkait analisis sentimen beserta dengan beberapa tahapan prosesnya. Berikut adalah tahapan analisis sentimen yang dilakukan dalam aplikasi ini:
-    """)
-
+    st.title("BERANDA")
+    st.write("SELAMAT DATANG DI APLIKASI ANALISIS SENTIMEN!!!")
     st.markdown('<p style="font-family: Times New Roman; font-size: 32px; font-weight: bold;">Pendeteksi Kata Slang</p>', unsafe_allow_html=True)
 
     # Menambahkan penjelasan mengenai aplikasi
